4f39ad3772924290b6f537200e1dd5a9c4a5c5dd
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-08-23 21:57:27 -0300

    project started and directories renamed

---
+++ frontend/service/weatherForecastService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { API_URL } from "/service/serviceSettings";
+
+class WeatherForecastService {
+    get() {
+        return axios.get(`${API_URL}rota_teste`).then((response) => {
+            return response.data;
+        }).catch(console.log);
+
+    }
+}
+
+export default new WeatherForecastService();
